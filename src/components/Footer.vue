f6196c182564c7047d1218f255a95517dd2e87a2
Ryoshikan <2365081663@qq.com>
2024-05-24 15:20:20 +0800

    test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,120 +1,56 @@
+<!-- 页脚 -->
+
 <template>
-  <footer id="footer" :class="store.footerBlur ? 'blur' : null">
-    <Transition name="fade" mode="out-in">
-      <div v-if="!store.playerState || !store.playerLrcShow" class="power">
-        <span>
-          Copyright&nbsp;&copy;
-          <span v-if="siteStartDate?.length >= 4" class="site-start">
-            {{ siteStartDate.substring(0, 4) }}
-            -
-          </span>
-          {{ fullYear }}
-          <a :href="siteUrl">{{ siteAnthor }}</a>
-        </span>
-        <!-- 以下信息请不要修改哦 -->
-        <span class="hidden">
-          &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{ config.author }}
-          </a>
-        </span>
-        <!-- 站点备案 -->
-        <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
-          &amp;
-          {{ siteIcp }}
-        </a>
-      </div>
-      <div v-else class="lrc">
-        <Transition name="fade" mode="out-in">
-          <div class="lrc-all" :key="store.getPlayerLrc">
-            <music-one theme="filled" size="18" fill="#efefef" />
-            <span class="lrc-text text-hidden" v-html="store.getPlayerLrc" />
-            <music-one theme="filled" size="18" fill="#efefef" />
-          </div>
-        </Transition>
-      </div>
-    </Transition>
-  </footer>
+    <a-row class="footer" type="flex" justify="center" align="middle" :gutter="2">
+        <!-- 版权 -->
+        <a-col :xs="24" :sm="8" :md="6" :lg="6" :xl="6"><Copyrights color="ghostwhite" :size="1"/></a-col>
+        <!-- 头像 -->
+        <a-col :xs="24" :sm="6" :md="4" :lg="4" :xl="4">
+            <a-popover arrowPointAtCenter>
+                <template slot="content"><Badge/></template>
+                <span class="avatar ant-avatar ant-avatar-circle ant-avatar-image">
+                    <img alt="林舍|Maner·Fan" draggable="false" src="../assets/avatar.jpg"/>
+                </span>
+            </a-popover>
+        </a-col>
+        <!-- 社交 -->
+        <a-col :xs="24" :sm="8" :md="6" :lg="6" :xl="6"><Social color="ghostwhite" :size="1.5" /></a-col>
+        <!-- 徽章 -->
+        <!-- 追求页面加载速度，放入Avatar中，这里暂时不展示 -->
+        <!-- <a-col :span="24"><Badge/></a-col> -->
+    </a-row>
 </template>
 
-<script setup>
-import { MusicOne } from "@icon-park/vue-next";
-import { mainStore } from "@/store";
-import config from "@/../package.json";
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import Copyrights from '@/components/footer/Copyrights.vue';
+    import Social from '@/components/footer/Social.vue';
 
-const store = mainStore();
-const fullYear = new Date().getFullYear();
-
-// 加载配置数据
-const siteStartDate = ref(import.meta.env.VITE_SITE_START);
-const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
-const siteAnthor = ref(import.meta.env.VITE_SITE_ANTHOR);
-const siteUrl = computed(() => {
-  const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "https://www.imsyy.top";
-  // 判断协议前缀
-  if (!url.startsWith("http://") && !url.startsWith("https://")) {
-    return "//" + url;
-  }
-  return url;
-});
+    @Component({
+        components: {
+            Copyrights,
+            Social,
+            Badge: () => import('@/components/footer/Badge.vue'),
+        },
+    })
+    export default class Footer extends Vue {
+    }
 </script>
 
-<style lang="scss" scoped>
-#footer {
-  width: 100%;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 46px;
-  line-height: 46px;
-  text-align: center;
-  z-index: 0;
-  font-size: 14px;
-  .power {
-    animation: fade 0.3s;
-  }
-  .lrc {
-    padding: 0 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    .lrc-all {
-      width: 98%;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      .lrc-text {
-        margin: 0 8px;
-      }
-      .i-icon {
-        width: 18px;
-        height: 18px;
-        display: inherit;
-      }
+<style scoped lang="scss">
+    @import '../styles/variable';
+
+    .footer {
+        &>div{
+            margin: 1rem auto;
+        }
+
+        background: #303030;
+
+        .avatar {
+            width: 60px;
+            height: 60px;
+            line-height: 60px;
+        }
     }
-  }
-  &.blur {
-    backdrop-filter: blur(10px);
-    background: rgb(0 0 0 / 25%);
-    font-size: 16px;
-  }
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.15s ease-in-out;
-  }
-  @media (max-width: 720px) {
-    font-size: 0.85rem;
-    &.blur {
-      font-size: 0.85rem;
-    }
-  }
-  @media (max-width: 480px) {
-    .hidden {
-      display: none;
-    }
-  }
-}
 </style>
